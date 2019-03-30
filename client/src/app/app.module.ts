be39b05a063e9e9d9c9a5662a401c0fc5ac1e8a9
Shefali Sharma <shefalisharmaspd@gmail.com>
2019-03-30 15:43:30 -0300

    chnages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BasePageComponent
   ],
   imports: [
     BrowserModule,
